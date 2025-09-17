868acac289ffbc35ab36912c06498db8eb0e0831
Khaiyum-Ahmed <sayedshakin666@gmail.com>
2025-09-17 12:33:53 +0600

    add post get and delete data server

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
-const app= express();
+const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -9,10 +10,64 @@ app.use(express.json())
 app.use(cors())
 
 
-app.get('',(req,res)=>{
+
+
+
+const uri = `mongodb+srv://${process.env.user}:${process.env.Pass}@usersmanagement.vsqrbwf.mongodb.net/?retryWrites=true&w=majority&appName=usersManagement`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+
+        const userCollections = client.db('UsersDB').collection('AnotherUsers');
+
+        // data receive from MDB
+        app.get('/users', async(req,res)=>{
+            const result = await userCollections.find().toArray();
+            res.send(result)
+        })
+
+        app.post('/users', async(req, res)=>{
+            const newUser = req.body;
+            console.log(newUser)
+            const result = await userCollections.insertOne(newUser);
+            res.send(result)
+        })
+
+        app.delete('/users/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await userCollections.deleteOne(query);
+            res.send(result)
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('', (req, res) => {
     res.send('Users management server in Running')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`User management server is running on PORT: ${port}`)
 })
